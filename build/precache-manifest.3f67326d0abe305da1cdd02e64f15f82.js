806f4bd6a70f1d61048efa5180a053c729079c3d
sfal <s.falchieri@gmail.com>
2019-12-06 18:18:46 +0100

    deploying react app

---
+++ build/precache-manifest.3f67326d0abe305da1cdd02e64f15f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95d6206a6324de43546f1298a7bd915",
+    "url": "/uniu-website/index.html"
+  },
+  {
+    "revision": "f761eb4742db6019427c",
+    "url": "/uniu-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dd71688eb9e650e9d6fb",
+    "url": "/uniu-website/static/js/2.bf74ecc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/uniu-website/static/js/2.bf74ecc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f761eb4742db6019427c",
+    "url": "/uniu-website/static/js/main.21f74fc9.chunk.js"
+  },
+  {
+    "revision": "0453594e2342c3ca6877",
+    "url": "/uniu-website/static/js/runtime-main.6bd9766c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uniu-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
