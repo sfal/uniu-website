ace0d082031f43ed4c87f943c638851e0d8abdc9
sfal <s.falchieri@gmail.com>
2019-12-26 13:46:34 +0100

    creating header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,14 @@
 import React, { Component } from "react";
 
-export default class Header extends React.Component {}
+export default class Header extends React.Component {
+  state = {};
+  componentDidMount() {}
+
+  render() {
+    return (
+      <header>
+        <h1>Header</h1>
+      </header>
+    );
+  }
+}
